16c5f8bf5d3503ba75a0587bd0e7992e9c9f54e1
Paul123111 <20102629wit@gmail.com>
2026-05-01 11:44:23 +0100

    ref: Add dynamic rotation option to LookSpriteSheet

--- PlanetHunter/Assets/Scripts/Player/LookSpriteSheet.cs
+++ PlanetHunter/Assets/Scripts/Player/LookSpriteSheet.cs
@@ -16,6 +16,8 @@ public class LookSpriteSheet : MonoBehaviour
     int headR = 120;
     int bodyR = 60;
 
+    [SerializeField] bool dynamicRotation = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -36,6 +38,9 @@ public class LookSpriteSheet : MonoBehaviour
         }
         anims[0].SetInteger("Direction", direction);
         anims[1].SetInteger("Direction", direction);
+
+        if (!dynamicRotation) return;
+
         float r = -1;
         float calc = 1;
         if (direction % 2 == 0) {
@@ -74,53 +79,6 @@ public class LookSpriteSheet : MonoBehaviour
         }
 
     }
-
-    // Update is called once per frame
-    //void Update()
-    //{
-    //    angle = GetMouseAngle();
-    //    direction = ((angle+45)/90)%4;
-    //    if (angle >= 270 && angle <= 285) {
-    //        direction = 1;
-    //    } else if (angle >= 75 && angle <= 105) {
-    //        direction = 3;
-    //    } else if (angle >= 255 && angle <= 75) {
-    //        direction = 2;
-    //    } else {
-    //        direction = 0;
-    //    }
-    //    anims[0].SetInteger("Direction", direction);
-    //    anims[1].SetInteger("Direction", direction);
-    //    Debug.Log("Angle: " + angle + ", Direction: " + direction);
-    //    if (direction % 2 == 0) {
-    //        //head.rotation = Quaternion.Euler(0, 0, ((angle + 45) % 90) - 45);
-    //        //head.localScale = new Vector3(myScale, myScale, 1f);
-    //        head.rotation = Quaternion.Euler(0, 0, (((angle + 45) % 90) - 45) / 4);
-    //        float scale = myScale - Mathf.Abs((((angle + 45) % 90) - 45) / 360f);
-    //        head.localScale = new Vector3(scale, myScale, 1f);
-    //        head.localPosition = new Vector3(((((angle + 45) % 90) - 45) / (360f * 4f)) - 0.073f, 0.45f, 0f);
-    //    } else {
-    //        head.rotation = Quaternion.Euler(0, 0, (((angle + 45) % 90) - 45)/4);
-    //        float scale = myScale - Mathf.Abs((((angle + 45) % 90) - 45) / 360f);
-    //        head.localScale  = new Vector3(scale, myScale, 1f);
-    //        head.localPosition = new Vector3(((((angle + 45) % 90) - 45) / (360f*4f))-0.073f, 0.45f, 0f);
-    //    }
-
-    //    if (direction % 2 == 0) {
-    //        //head.rotation = Quaternion.Euler(0, 0, ((angle + 45) % 90) - 45);
-    //        //head.localScale = new Vector3(myScale, myScale, 1f);
-    //        body.rotation = Quaternion.Euler(0, 0, (((angle + 45) % 90) - 45) / 4);
-    //        float scale = bodyScale - Mathf.Abs((((angle + 45) % 90) - 45) / 360f);
-    //        body.localScale = new Vector3(scale, bodyScale, 1f);
-    //        body.localPosition = new Vector3(((((angle + 45) % 90) - 45) / (360f * 1f)), 0f, 0f);
-    //    } else {
-    //        body.rotation = Quaternion.Euler(0, 0, (((angle + 45) % 90) - 45) / 8);
-    //        float scale = bodyScale - Mathf.Abs((((angle + 45) % 90) - 45) / 360f);
-    //        body.localScale = new Vector3(scale, bodyScale, 1f);
-    //        body.localPosition = new Vector3(((((angle + 45) % 90) - 45) / (360f * 2f)), 0f, 0f);
-    //    }
-
-    //}
 
     int GetMouseAngle() {
         Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
