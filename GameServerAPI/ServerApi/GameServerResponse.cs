103fad03b28cb6456b7291da33e29e2897ebdc4b
Paul123111 <20102629wit@gmail.com>
2026-05-06 10:47:07 +0100

    feat: return GameServerResponse in API

--- GameServerAPI/ServerApi/GameServerResponse.cs
+++ GameServerAPI/ServerApi/GameServerResponse.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 public record GameServerResponse(
@@ -7,3 +8,19 @@ public record GameServerResponse(
     [property: JsonPropertyName("players")] int Players,
     [property: JsonPropertyName("capacity")] int Capacity
 );
+
+public static class GameServerResponseUtils {
+    public static GameServerResponse ParseJson(JsonElement item) {
+        var status = item.GetProperty("status");
+
+        var ip = status.GetProperty("ip").GetString();
+        var port = status.GetProperty("port").GetInt32();
+        var state = status.GetProperty("state").GetString();
+        
+        var players = status.GetProperty("players");
+        var count = players.GetProperty("count").GetInt32();
+        var capacity = players.GetProperty("capacity").GetInt32();
+
+        return new GameServerResponse(ip, port, state, count, capacity);
+    }
+}
