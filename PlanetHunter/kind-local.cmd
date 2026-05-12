f1890598d14c6d9c7b10f90f0d50e0b32df9571b
Paul123111 <20102629wit@gmail.com>
2026-05-12 19:13:56 +0100

    ref: remove obsolete files and folders in PlanetHunter and remove KubernetesConfig

--- PlanetHunter/kind-local.cmd
+++
@@ -1,7 +0,0 @@
-kubectl delete gs --all
-make build-image
-kind load docker-image planet-hunter/simple-server/unity-simple-server:0.2
-kubectl create -f gameserver.yaml
-POD_NAME=kubectl get pods -o name --no-headers=true
-kubectl port-forward "$POD_NAME" 7000:7000
-
