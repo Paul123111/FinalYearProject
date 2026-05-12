fa7b051e653dce6e9d3c87f93cdd0d5fe5eafaa3
Paul123111 <20102629wit@gmail.com>
2026-05-12 19:17:51 +0100

    fix: correctly removed KubernetesConfig

--- KubernetesConfig/minikube-start.cmd
+++
@@ -1,5 +0,0 @@
-minikube delete
-minikube start --ports 7000-7100:7000-7100/udp
-
-kubectl create namespace agones-system
-kubectl apply --server-side -f agones-install.yaml
--- KubernetesConfig/script.cmd
+++
@@ -1,5 +0,0 @@
-kind delete cluster
-kind create cluster --config ports.yaml
-
-kubectl create namespace agones-system
-kubectl apply --server-side -f agones-install.yaml
