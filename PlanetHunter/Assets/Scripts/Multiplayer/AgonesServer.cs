22a457c008335fa4822d20ea40ac4fd8a8f58053
Paul123111 <20102629wit@gmail.com>
2026-05-13 21:23:23 +0100

    feat: rework networking auth in menu + add spinning planet to level select

--- PlanetHunter/Assets/Scripts/Multiplayer/AgonesServer.cs
+++ PlanetHunter/Assets/Scripts/Multiplayer/AgonesServer.cs
@@ -1,56 +1,43 @@
-﻿// Copyright 2019 Google LLC
-// All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-using Agones;
-using UnityEngine;
+﻿using Agones;
 using Mirror;
-using TMPro;
+using UnityEngine;
 
-namespace AgonesExample {
-    [RequireComponent(typeof(AgonesAlphaSdk))]
-    public class AgonesServer : MonoBehaviour {
+#if UNITY_EDITOR
+using ParrelSync;
+#endif
+
+[RequireComponent(typeof(AgonesAlphaSdk))]
+public class AgonesServer : MonoBehaviour {
         
-        private AgonesAlphaSdk agones = null;
+    private AgonesAlphaSdk agones;
 
-        async void Start() {
-            agones = GetComponent<AgonesAlphaSdk>();
-            bool ok = await agones.Connect();
-            if (ok) {
-                Debug.Log(("Server - Connected"));
-            } else {
-                Debug.Log(("Server - Failed to connect, exiting"));
-                Application.Quit(1);
-            }
+#if UNITY_EDITOR || !UNITY_SERVER
+    void Awake() {
+        if (!ClonesManager.IsClone()) {
+            Destroy(gameObject);
+        }
+    }
+#endif
 
-            ok = await agones.Ready();
-            await agones.SetPlayerCapacity(4);
-            if (ok) {
-                Debug.Log($"Server - Ready");
-            } else {
-                Debug.Log($"Server - Ready failed");
-                Application.Quit();
-            }
+    async void Start() {
+        agones = GetComponent<AgonesAlphaSdk>();
+        bool ok = await agones.Connect();
+        if (ok) {
+            Debug.Log(("Server - Connected"));
+        } else {
+            Debug.Log(("Server - Failed to connect, exiting"));
+            Application.Quit(1);
         }
 
-        //void OnDestroy() {
-        //    //bool ok = await agones.Shutdown();
-        //    //if (ok) {
-        //    //    Debug.Log("Server - Close");
-        //    //} else {
-        //    //    Debug.Log("Server did not shutdown properly");
-        //    //}
-        //}
+        ok = await agones.Ready();
+        await agones.SetPlayerCapacity(4);
+        if (ok) {
+            Debug.Log($"Server - Ready");
+        } else {
+            Debug.Log($"Server - Ready failed");
+            Application.Quit();
+        }
+
+        NetworkManager.singleton.StartServer();
     }
 }
