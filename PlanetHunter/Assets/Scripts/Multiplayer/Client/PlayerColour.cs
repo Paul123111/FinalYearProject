13537822ae3b196eead9be5e174c0e369caf8e3e
Paul123111 <20102629wit@gmail.com>
2026-05-15 15:18:27 +0100

    feat: Astronaut player colour + planet select

--- PlanetHunter/Assets/Scripts/Multiplayer/Client/PlayerColour.cs
+++ PlanetHunter/Assets/Scripts/Multiplayer/Client/PlayerColour.cs
@@ -11,13 +11,21 @@ public class PlayerColour : NetworkBehaviour
     MaterialPropertyBlock propBlock;
     Color[] colourScheme;
 
+    // only need two playerColour types
+    [SerializeField] bool isRocket;
+    Dictionary<long, string[]> hexMap;
+
     private void Awake() {
         myRenderer = GetComponent<SpriteRenderer>();
         propBlock = new MaterialPropertyBlock();
+        hexMap = PlayerColourSchemes.entityHexMap;
+        if (isRocket) {
+            hexMap = PlayerColourSchemes.rocketHexMap;
+        }
     }
 
     void OnColourChanged(long oldValue, long newValue) {
-        colourScheme = PlayerColourSchemes.GetColorScheme(newValue);
+        colourScheme = PlayerColourSchemes.GetColorScheme(newValue, hexMap);
         myRenderer.GetPropertyBlock(propBlock);
         propBlock.SetColor("_NewColour", colourScheme[0]);
         propBlock.SetColor("_NewOutline", colourScheme[1]);
@@ -26,23 +34,31 @@ public class PlayerColour : NetworkBehaviour
     }
 }
 
-// Colour schemes have two colours
+// Colour schemes have two or three colours
 //   - index 0: a fill colour
 //   - index 1: an outline colour
+//   - index 2: a face colour (for astronaut)
 public static class PlayerColourSchemes {
-    public static readonly Dictionary<long, string[]> hexMap = new Dictionary<long, string[]> {
+    public static readonly Dictionary<long, string[]> rocketHexMap = new Dictionary<long, string[]> {
         { 1, new string[] { "#009cfc", "#0c08ec" } }, // blue, dark blue
         { 2, new string[] { "#e41800", "#680008" } }, // red, dark red
         { 3, new string[] { "#24d000", "#004c00" } }, // green, dark green
         { 4, new string[] { "#fcc400", "#884c00" } }, // yellow, light brown
     };
-    public static Color[] GetColorScheme(long playerNum) {
+    public static readonly Dictionary<long, string[]> entityHexMap = new Dictionary<long, string[]> {
+        { 1, new string[] { "#0098fc", "#000060", "#7cfcfc" } }, // blue, dark blue
+        { 2, new string[] { "#e41800", "#480014", "#fc2800" } }, // red, dark red
+        { 3, new string[] { "#24d000", "#002400", "#c0f800" } }, // green, dark green
+        { 4, new string[] { "#fcc400", "#602400", "#fce94f" } }, // yellow, light brown
+    };
+    public static Color[] GetColorScheme(long playerNum, Dictionary<long, string[]> hexMap) {
+        int len = hexMap[1].Length;
         string[] hexes;
-        Color[] colours = new Color[2];
+        Color[] colours = new Color[len];
         Debug.Log(playerNum);
         if (hexMap.TryGetValue(playerNum, out hexes)) {
             for (int i = 0; i < colours.Length; i++) {
-                if (hexes.Length == 2 && ColorUtility.TryParseHtmlString(hexes[i], out Color color)) {
+                if (hexes.Length == len && ColorUtility.TryParseHtmlString(hexes[i], out Color color)) {
                     colours[i] = color;
                 } else {
                     Debug.LogError("Colour was not found or hexMap did not return exactly two hexes");
