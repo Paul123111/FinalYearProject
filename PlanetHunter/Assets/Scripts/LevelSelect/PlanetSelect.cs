53a128c956f211290259ef06f08b6aa106dcccf7
Paul123111 <20102629wit@gmail.com>
2026-05-15 18:00:45 +0100

    feat: rework multiplayer rocket and add voting for planets to level select

--- PlanetHunter/Assets/Scripts/LevelSelect/PlanetSelect.cs
+++ PlanetHunter/Assets/Scripts/LevelSelect/PlanetSelect.cs
@@ -1,5 +1,5 @@
 using Mirror;
-using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,40 +9,18 @@ public class PlanetSelect : NetworkBehaviour
 
     bool nearbyLocal = false;
     [SyncVar(hook = nameof(OnPlayerNearby))] bool nearby = false;
+    public bool selected { get { return nearby; } } 
 
     HashSet<GameObject> playersNearby = new HashSet<GameObject>();
 
-    //[Server]
-    //bool PlayersNearby() {
-    //    foreach (var conn in NetworkServer.connections.Values) {
-    //        if (conn.identity != null) {
-    //            GameObject player = conn.identity.gameObject;
-    //            if (Vector3.Distance(player.transform.position, transform.position) <= radius) {
-    //                return true;
-    //            }
-    //        }
-    //    }
-    //    return false;
-    //}
-
     void OnPlayerNearby(bool oldValue, bool newValue) {
         outline.SetActive(newValue);
+        NetworkClient.localPlayer.GetComponent<NetworkPlayer>().VoteForLevel(this, !newValue);
     }
-
-    //private void Update() {
-    //    if (!NetworkServer.active) return;
-    //    bool n = PlayersNearby();
-    //    if (n != nearby) {
-    //        nearby = n;
-    //    }
-    //}
 
     [ServerCallback]
     private void OnTriggerEnter2D(Collider2D other) {
         NetworkIdentity identity = other.GetComponent<NetworkIdentity>();
-        Debug.Log(identity);
-        Debug.Log(identity?.gameObject);
-        Debug.Log(identity?.gameObject?.tag);
         if (identity?.gameObject?.tag == "Player") {
             playersNearby.Add(identity.gameObject);
             nearbyLocal = playersNearby.Count > 0;
---
+++ PlanetHunter/Assets/Scripts/LevelSelect/StartPlanet.cs
@@ -0,0 +1,86 @@
+using Mirror;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartPlanet : NetworkBehaviour
+{
+    [SerializeField] Image countdownUI;
+    TextMeshProUGUI countdownText;
+    //[SyncVar(hook = nameof(OnCountdownStart))]
+    bool startedCountdown = false;
+
+    [SyncVar] double startTime;
+    readonly SyncList<PlanetSelect> voteList = new SyncList<PlanetSelect>();
+    [SerializeField] double countdownLength;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        countdownText = countdownUI.GetComponentInChildren<TextMeshProUGUI>();
+
+        startedCountdown = false;
+        countdownUI.enabled = false;
+        countdownText.enabled = false;
+    }
+
+    public override void OnStartServer() {
+        voteList.OnAdd += OnVoteAdd;
+        voteList.OnRemove += OnVoteRemove;
+    }
+
+    private void Update() {
+        if (startedCountdown) {
+            countdownText.text = $"Start in {Mathf.CeilToInt((float)(startTime + countdownLength-NetworkTime.time))}...";
+            if (NetworkTime.time > startTime+countdownLength) {
+                int index = Random.Range(0, voteList.Count);
+                countdownText.text = $"Landing on {voteList[index]}!";
+                startedCountdown = false;
+            }
+        }
+    }
+
+    [Server]
+    public void CountdownStart() {
+        if (startedCountdown) return;
+        startedCountdown = true;
+        startTime = NetworkTime.time;
+        CountdownUIClient(true);
+    }
+
+    [Server]
+    public void CountdownStop() {
+        startedCountdown = false;
+        CountdownUIClient(false);
+    }
+
+    [Server]
+    public void VoteForPlanet(PlanetSelect vote, bool removeVote) {
+        if (!removeVote) {
+            voteList.Add(vote);
+        } else {
+            voteList.Remove(vote);
+        }
+    }
+
+    [ClientRpc]
+    void CountdownUIClient(bool start) {
+        startedCountdown = start;
+        countdownUI.enabled = start;
+        countdownText.enabled = start;
+    }
+
+    [Server]
+    void OnVoteAdd(int vote) {
+        if (voteList.Count > 0) {
+            CountdownStart();
+        }
+    }
+
+    [Server]
+    void OnVoteRemove(int vote, PlanetSelect p) {
+        if (voteList.Count == 0) {
+            CountdownStop();
+        }
+    }
+}
