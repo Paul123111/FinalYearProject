dc325351f6f828ad33266b0ed1fe5b5806f36c64
Paul123111 <20102629wit@gmail.com>
2026-05-15 22:56:32 +0100

    feat: Add individual cameras to astronauts

--- PlanetHunter/helperScripts/startup.bat
+++ PlanetHunter/helperScripts/startup.bat
@@ -1,5 +1,5 @@
-start ../Builds/ClientGame/UnitySimpleClient.exe -profile Player1 -screen-width 640 -screen-height 480
-start ../Builds/ClientGame/UnitySimpleClient.exe -profile Player2 -screen-width 640 -screen-height 480
-start ../Builds/ClientGame/UnitySimpleClient.exe -profile Player3 -screen-width 640 -screen-height 480
-start ../Builds/ClientGame/UnitySimpleClient.exe -profile Player4 -screen-width 640 -screen-height 480
+start ../Builds/PlanetHunter/PlanetHunterClient.exe -profile Player1 -screen-width 640 -screen-height 480
+start ../Builds/PlanetHunter/PlanetHunterClient.exe -profile Player2 -screen-width 640 -screen-height 480
+start ../Builds/PlanetHunter/PlanetHunterClient.exe -profile Player3 -screen-width 640 -screen-height 480
+start ../Builds/PlanetHunter/PlanetHunterClient.exe -profile Player4 -screen-width 640 -screen-height 480
 
